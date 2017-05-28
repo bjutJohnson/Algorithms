88d461e797eec84123d841b2b66ba1bfb69f07cd
Johnson Chen <juncheng@bjut.edu.cn>
2017-05-28 21:45:20 +0800

    update

--- src/main.go
+++ src/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-<<<<<<< HEAD
+
 	//"graph"
 	"greedy"
-=======
+
 	"graph"
 	//"fmt"
 	//"sync"
 	//"johnson_utility"
->>>>>>> 1946829fbd3158493b9878f65174816c040bcc72
 )
 
 func main() {
@@ -21,9 +20,9 @@ func main() {
 	//recursion.TestDivideInteger()
 	//recursion.TestHannoi()
 	//graph.Test_DFS()
-<<<<<<< HEAD
+
 	greedy.TestLeastCoinNum()
-=======
+
 	graph.Test_Top()
->>>>>>> 1946829fbd3158493b9878f65174816c040bcc72
+
 }
